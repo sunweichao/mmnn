b8ee53bea6cd118ed66c3fb1557c3f487b299ff4
sunweichao <124552242@qq.com>
2016-10-28 13:45:49 +0800

    创建工程

---
+++ sdaa/sdaa/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sdaa
+//
+//  Created by 孙玮超 on 16/10/28.
+//  Copyright © 2016年 GZ_lanou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ sdaa/sdaa/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  sdaa
+//
+//  Created by 孙玮超 on 16/10/28.
+//  Copyright © 2016年 GZ_lanou. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
